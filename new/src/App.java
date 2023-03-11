8d7e8e88a7415f6909d9e79dd7c6c613d354e137
danhvuba <danhvuba@gmail.com>
2023-03-11 20:14:58 +0300

    4.3

--- new/src/App.java
+++ new/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // input
+        System.out.print("enter 2 numbers: ");
+        int x;
+        int y;
+        try (Scanner input = new Scanner(System.in)) {
+            x = input.nextInt();
+            y = input.nextInt();
+        }
+        int max = (x > y) ? x : y;
+        System.out.println("max = " + max);
+
+        // array
+        char[] arr = { 'a', 'b', 'd' };
+        for (char c : arr) {
+            System.out.println(c);
+        }
+
     }
 }
